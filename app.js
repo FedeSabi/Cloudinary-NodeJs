cc4afe22cb2875d56f62dc37a0d2ea028ecccfea
Federico Sabiche <fsabiche>
2023-11-03 15:27:37 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require ('express')
+const app = express()
+require('dotenv').config()
+const multer = require('multer')
+const cloudinary = require('cloudinary').v2
+
+const upload = multer({ dest: 'uploads' })
+
+const port = 3000
+
+app.get('/', (req,res) =>{
+    res.send('prueba')
+})
+
+app.post('/upload',upload.single('image'), async (req,res) =>{
+try{
+    const result = await cloudinary.uploader.upload(req.file.path)
+    res.status(200).json(result)
+}catch(error){
+    console.log(error.error)
+    res.status(400).send(error.message)
+}
+})
+
+app.listen(port, ()=> {
+    console.log('Running in port ', port)
+})
